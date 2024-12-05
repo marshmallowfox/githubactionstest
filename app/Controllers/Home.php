7a4cfcc56c8228bdc80efc110b84155fb0d7c0cd
marshmallowfox <shudaof@gmail.com>
2024-12-05 04:31:04 +0000

    Intial Commit

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,30 +4,8 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index
-    (
-    )
-    : string
+    public function index(): string
     {
-        print_r(
-            'xxx');
-
-        print_r
-        (
-            '
-        fan');
-
-        print_r
-        (
-            '
-        said');
-
-        print_r
-        (
-            '
-        fail');
-
-
         return view('welcome_message');
     }
 }
