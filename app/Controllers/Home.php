dade5faf73d0f8e70cbe19865e62ffd5bd7cbca5
marshmallowfox <shudaof@gmail.com>
2024-12-05 12:16:39 +0600

    test

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,27 +7,7 @@ class Home extends BaseController
     public function index(
     ): string {
         print_r(
-            'xxx'
-        );
-
-        print_r(
-            '
-        fan'
-        );
-
-        print_r(
-            '
-        said'
-        );
-
-        print_r(
-            '
-        fail'
-        );
-
-        print_r(
-            '
-        ok'
+            'far'
         );
 
         return view('welcome_message');
