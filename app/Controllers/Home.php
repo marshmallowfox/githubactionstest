a51fce30c9181d24666bdd8fedc510fa7dc1ce94
marshmallowfox <shudaof@gmail.com>
2024-12-05 11:03:12 +0600

    test

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,7 +4,10 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function index(
+
+    )
+    : string
     {
         return view('welcome_message');
     }
